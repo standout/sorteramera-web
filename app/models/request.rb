c8d588ea55a737cdc3c75a2c2926cf2447e0e29d
Emric <emric.mansson@gmail.com>
2016-11-01 12:08:12 +0100

    Resolved conflicts

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,6 +1,5 @@
 class Request < ActiveRecord::Base
   def self.post(path, body, content_type = "json")
-<<<<<<< HEAD
     http_req(path, "post", body.to_json)
   end
 
@@ -10,17 +9,14 @@ class Request < ActiveRecord::Base
 
   def self.put(path, body, content_type = "json")
     http_req(path, "put", body)
-=======
-    res = http_req(path, "post", body.to_json)
   end
 
   def self.get(path, content_type = "json")
-    res = http_req(path, "get")
+    http_req(path, "get")
   end
 
   def self.put(path, body, content_type = "json")
-    res = http_req(path, "put", body)
->>>>>>> 1e1337c7438d6bc2e47e4e1bb78685d8f7489554
+    http_req(path, "put", body)
   end
 
   def self.http_req(path, type, body = "", content_type = "json")
@@ -47,11 +43,7 @@ class Request < ActiveRecord::Base
     end
 
     # Return response
-<<<<<<< HEAD
     Net::HTTP::start(uri.hostname, uri.port) {|http|
-=======
-    res = Net::HTTP::start(uri.hostname, uri.port) {|http|
->>>>>>> 1e1337c7438d6bc2e47e4e1bb78685d8f7489554
       http.request(req)
     }
   end
